c59a4872860ebbf5897159abff456b2e8536cf42
RainhaGomez <gyhsantos2009@gmail.com>
2025-08-12 19:15:42 -0300

    Add files via upload
    Modulo 1 Completo

---
+++ 01 - FundamentosdePythons.py
@@ -0,0 +1,20 @@
+#A Função PRINT imprime algo na tela para o usuario
+print('Aula de python')
+
+'''''
+#Erro de identação - não pode ter espaços ou tabulação  sem necessidade
+print('Aula de python')
+  print('Aula de python')
+
+# Só imprime o texto se estiver entre aspas
+print(Aula)
+
+#Deve fechar a () Sempre
+print('Python'
+
+#Não deve ter erros nas variáveis
+Pint('Aula')
+
+#Case sensitive - diferencia maiusculo do minusculo
+PRINT('Ola')
+'''''
