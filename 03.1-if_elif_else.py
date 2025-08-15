15dcb870c5f61a17e9ddb615a6c3edd054b0861e
RainhaGomez <gyhsantos2009@gmail.com>
2025-08-14 21:47:36 -0300

    Update 03.1-if_elif_else.py

--- 03.1-if_elif_else.py
+++ 03.1-if_elif_else.py
@@ -37,3 +37,119 @@ if numero >= 0:
 else:
   print('Número Negativo')
   
+'''
+Desafio
+2. Valor da Parcela
+Faça um algoritimo que receba um valor de uma compra e receba o número de prestações, apresente o valor das prestações sem juros.
+'''
+valor_compra = float(input('Digite o valor da compra:\n'))
+qtd_parcela = int(input('Digite o número de parcelas:\n'))
+print(f'O valor da compra R${valor_compra} parcelada em {qtd_parcela:.2f} vezes')
+
+'''
+Desafio
+3. Maior entre dois números
+Crie um programa que peça ao usuário para digitar dois números. O programa deve comparar os dois números e exibir uma mensagem indicando qual deles é o maior
+'''
+num1 = float(input('Digite o primeiro número:\n'))
+num2 = float(input('Digite o segundo número:\n'))
+if num1 > num2:
+  print(f'O maior número é {num1}')
+else:
+  print(f' O maior número é {num2}')
+
+#Outro jeito de fazer sem i if
+#print(f'O maior número é {max(num1,num2)}')
+
+# EXEMPLO DE USO COM ELIF
+
+# Notas Escolares
+nota = 8
+if nota >=7:
+  print('Aprovado')
+elif nota <5:# O Elif sempre deve estar no meio do if e o else
+  print('Reprovado')
+else:
+  print('Recuperação')
+
+
+# Pagamento Simples
+#If para começar uma ação, Elif para para outra oção da ação e else para terminar a ação referente ao If. Entre o if e o Else pode ter várias Elif
+menu = input('1 - Pagamento\n 2- sair:\n')
+if menu == '1':
+  pagamento = input('1 - Cartão Débito\n 2 - Cartão de Crédito\n 3 - Boleto\n 4 - Pix\n')
+  if pagamento =='1':
+    print('Cartão de Débito')
+  elif pagamento =='2':
+    print('Cartão de Crédito 2x')
+  elif pagamento=='3':
+    print('Boleto')
+  elif pagamento =='4':
+    print('Pix')
+  else:
+    print('Opção Inválida!')
+elif menu =='2':
+  print('Saiu')
+else:
+  print('opção Inválida!')
+
+'''
+Desafio
+4. Maior entre dois números
+Crie um programa que peça ao usuário para digitar dois números. O programa deve comparar os dois números e exibir uma mensagem indicando qual deles é o maior ou se são iguais
+'''
+num1 = float(input('Digite o primeiro número:\n'))
+num2 = float(input('Digite o segundo número:\n'))
+if num1 > num2:
+  print(f'O maior número é {num1}')
+elif num2 > num1:
+  print(f' O maior número é {num2}')
+else:
+  print(f'Esses números são iguais ')
+
+#Outro jeito de fazer sem i if
+#print(f'O maior número é {max(num1,num2)}')
+
+'''
+Desafio
+
+5. Calculadora
+Exiba um menu de oprações para o usuário:
+Soma (+)
+Subtração (-)
+Multiplicação (*)
+Divisão (/)
+Sair (0)
+'''
+'''
+numeroa=float(input('Digite o primeiro número '))
+numerob = float(input('Digite o segundo número '))
+op1=input('Digite o operador desejável, (+), (-), (*) e (/)\n')
+if op1 == '+':
+  print(f'O resultado é:\n {numeroa+numerob:.2f}')
+elif op1== '-':
+  print(f'O resultado é:\n {numeroa-numerob:.2f}')
+elif op1== '*':
+  print(f'O resultado é:\n {numeroa*numerob:.2f}')
+elif op1== '/':
+  print(f'O resultado é:\n {numeroa/numerob:.2f}')
+else:
+  print(f'SAIR')
+  Aqui dará um erro na divisão por zero, pois no python não é possível.
+  '''
+numeroa=float(input('Digite o primeiro número '))
+numerob = float(input('Digite o segundo número '))
+op1=input('Digite o operador desejável, (+), (-), (*) e (/)\n')
+if op1 == '+' or op1 == 'soma':
+  print(f'O resultado é:\n {numeroa+numerob:.2f}')
+elif op1== '-'or op1 == 'subtração':
+  print(f'O resultado é:\n {numeroa-numerob:.2f}')
+elif op1== '*'or op1 == 'multiplicação':
+  print(f'O resultado é:\n {numeroa*numerob:.2f}')
+elif op1== '/'or op1 == 'divisão':
+  if numeroa == 0: 
+    print(f'O Resultado não é possível:\n ')
+  elif numerob == 0:
+    print(f'O resultado é:\n {numeroa/numerob:.2f}')
+else:
+  print(f'SAIR')
